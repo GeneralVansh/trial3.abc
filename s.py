b37dc853d4948c4e88073841c1baa1e4c16fa9e5
GeneralVansh <118705634+GeneralVansh@users.noreply.github.com>
2025-10-26 22:58:47 +0530

    Delete s.py

--- s.py
+++
@@ -1,9 +0,0 @@
-from gensim.models import KeyedVectors
-
-# apna file path yahan daal
-model_path = r"C:\Users\<your_username>\Downloads\GoogleNews-vectors-negative300.bin"
-
-model = KeyedVectors.load_word2vec_format(model_path, binary=True)
-
-print("Model loaded successfully!")
-print(model.most_similar("king"))
